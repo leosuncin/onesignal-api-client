a6b5855a7663b653a467805e80ae568a8b88b17c
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2018-01-18 00:40:49 -0600

    fix(api): fix IDevices interface
    Unset max array sizes

--- lib/interfaces/notification/IDevices.ts
+++ lib/interfaces/notification/IDevices.ts
@@ -9,47 +9,47 @@ export interface IDevices {
    * Specific players to send your notification to.
    * Do not combine with other targeting parameters. Not compatible with any other targeting parameters.
    */
-  include_player_ids: string[2000];
+  include_player_ids: string[];
   /**
    * Target using iOS device tokens.
    *
    * @deprecated NOT RECOMMENDED - Please consider using `include_player_ids` instead.
    */
-  include_ios_tokens?: string[2000];
+  include_ios_tokens?: string[];
   /**
    * Target using Windows Phone 8.0 URIs.
    *
    * @deprecated NOT RECOMMENDED - Please consider using `include_player_ids` instead.
    */
-  include_wp_urls?: string[2000];
+  include_wp_urls?: string[];
   /**
    * Target using Windows Phone 8.1 URIs.
    *
    * @deprecated NOT RECOMMENDED - Please consider using `include_player_ids` instead.
    */
-  include_wp_wns_uris?: string[2000];
+  include_wp_wns_uris?: string[];
   /**
    * Target using Chrome Web Push registration IDs.
    *
    * @deprecated NOT RECOMMENDED Please consider using `include_player_ids` instead.
    */
-  include_amazon_reg_ids?: string[2000];
+  include_amazon_reg_ids?: string[];
   /**
    * Target using Chrome App registration IDs.
    *
    * @deprecated NOT RECOMMENDED Please consider using `include_player_ids` instead.
    */
-  include_chrome_reg_ids?: string[2000];
+  include_chrome_reg_ids?: string[];
   /**
    * Target using Chrome Web Push registration IDs.
    *
    * @deprecated NOT RECOMMENDED Please consider using `include_player_ids` instead.
    */
-  include_chrome_web_reg_ids?: string[2000];
+  include_chrome_web_reg_ids?: string[];
   /**
    * Target using Android device registration IDs.
    *
    * @deprecated NOT RECOMMENDED Please consider using `include_player_ids` instead.
    */
-  include_android_reg_ids?: string[2000];
+  include_android_reg_ids?: string[];
 }
